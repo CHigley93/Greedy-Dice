f8a98ae36741a26d1ccb9ee25ef2179a0954c905
chigl <chigl@192.168.1.155>
2019-08-30 19:03:50 -0500

    changed dice to an array and other minor changes

--- src/main.java
+++ src/main.java
@@ -5,57 +5,50 @@ import java.util.Scanner;
 public class main {
 	public static void main(String args[]) {
 		Random rand = new Random();
-		int die1 = rand.nextInt(6) + 1;
-		int die2 = rand.nextInt(6) + 1;
-		int die3 = rand.nextInt(6) + 1;
-		int die4 = rand.nextInt(6) + 1;
-		int die5 = rand.nextInt(6) + 1;
-		int die6 = rand.nextInt(6) + 1;
-		int score = die1 + die2 + die3 + die4 + die5 + die6;
-
 		Scanner in = new Scanner(System.in);
-		int i, rerollNum;
+		int dice[] = new int[6];
+		int i, j, rerollNum, score = 0;
 
-		System.out.println("Your rolls are: " + die1 + " " + die2 + " " + die3 + " " + die4 + " " + die5 + " " + die6);
-		System.out.println("Your score is : " + score);
-		System.out.println("How many dice would you like to reroll?");
-
-		rerollNum = in.nextInt();
-		int reroll[] = new int[rerollNum];
-
-		for (i = 0; i < rerollNum; i++) {
-			System.out.println("Enter a die to reroll");
-			reroll[i] = in.nextInt();
+		for (i = 0; i < dice.length; i++) {
+			dice[i] = rand.nextInt(6) + 1;
+			score += dice[i];
 		}
 
-		System.out.println("You chose to reroll die: " + Arrays.toString(reroll));
-		for (i = 0; i < rerollNum; i++) {
-			switch (reroll[i]) {
-			case 1:
-				die1 = rand.nextInt(6) + 1;
-				break;
-			case 2:
-				die2 = rand.nextInt(6) + 1;
-				break;
-			case 3:
-				die3 = rand.nextInt(6) + 1;
-				break;
-			case 4:
-				die4 = rand.nextInt(6) + 1;
-				break;
-			case 5:
-				die5 = rand.nextInt(6) + 1;
-				break;
-			case 6:
-				die6 = rand.nextInt(6) + 1;
-				break;
-			default:
-				System.out.println("That's not an option dummy");
+		System.out.println("Your rolls are: " + Arrays.toString(dice));
+		System.out.println("Your score is : " + score);
+
+		for (j = 0; j < 3; j++) {
+
+			do {
+			System.out.println("How many dice would you like to reroll?");
+			rerollNum = in.nextInt();
+			if(rerollNum == 0) {
 				break;
 			}
+			else if(rerollNum > 6 || rerollNum < 0){
+				System.out.println("You can't reroll that many dice");
+			}
+			}
+			while(rerollNum <=6);
+			int reroll[] = new int[rerollNum];
+			
+
+			for (i = 0; i < rerollNum; i++) {
+				System.out.println("Enter a die to reroll");
+				reroll[i] = in.nextInt();
+			}
+
+			System.out.println("You chose to reroll dice: " + Arrays.toString(reroll));
+			for (i = 0; i < rerollNum; i++) {
+				dice[reroll[i] - 1] = rand.nextInt(6) + 1;
+			}
+			score = 0;
+			for (i = 0; i < dice.length; i++) {
+				score += dice[i];
+			}
+			System.out.println("Your rolls are: " + Arrays.toString(dice));
+			System.out.println("Your score is : " + score);
 		}
-		score = die1 + die2 + die3 + die4 + die5 + die6;
-		System.out.println("Your rolls are: " + die1 + " " + die2 + " " + die3 + " " + die4 + " " + die5 + " " + die6);
-		System.out.println("Your score is : " + score);
+		System.out.println("Your final score is:" + score);
 	}
 }
