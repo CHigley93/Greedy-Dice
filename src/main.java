dbaf79327c2dd569d7d4b0de6dda93a251e0002a
Addison Higley <addison.higley@gmail.com>
2019-08-30 19:46:56 -0500

    try except for bad input

--- src/main.java
+++ src/main.java
@@ -3,6 +3,31 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class main {
+	public static Integer get_dice_num(String input, Scanner ins, Boolean zeroes) {
+		try {
+			int result = Integer.parseInt(input);
+			if(zeroes) {
+				if(result < 0 || result > 6) {
+					throw new IllegalArgumentException();
+				}
+			}
+			else if(result < 1 || result > 6) {
+				throw new IllegalArgumentException();
+			}
+			return result;
+		}
+		catch(Exception e) {
+			if(zeroes) {
+				System.out.println("You must enter a number 0-6");
+			}
+			else {
+				System.out.println("You must enter a number 1-6");
+			}
+			return get_dice_num(ins.nextLine(), ins, zeroes);
+		}
+	}
+
+	
 	public static void main(String args[]) {
 		Random rand = new Random();
 		Scanner in = new Scanner(System.in);
@@ -13,29 +38,22 @@ public class main {
 			dice[i] = rand.nextInt(6) + 1;
 			score += dice[i];
 		}
-
 		System.out.println("Your rolls are: " + Arrays.toString(dice));
 		System.out.println("Your score is : " + score);
 
 		for (j = 0; j < 3; j++) {
-
-			do {
+			
 			System.out.println("How many dice would you like to reroll?");
-			rerollNum = in.nextInt();
+			rerollNum = get_dice_num(in.nextLine(), in, true);
 			if(rerollNum == 0) {
 				break;
 			}
-			else if(rerollNum > 6 || rerollNum < 0){
-				System.out.println("You can't reroll that many dice");
-			}
-			}
-			while(rerollNum <=6);
 			int reroll[] = new int[rerollNum];
 			
 
 			for (i = 0; i < rerollNum; i++) {
 				System.out.println("Enter a die to reroll");
-				reroll[i] = in.nextInt();
+				reroll[i] = get_dice_num(in.nextLine(), in, false);
 			}
 
 			System.out.println("You chose to reroll dice: " + Arrays.toString(reroll));
